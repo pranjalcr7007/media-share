8ff897fa9fca279cd30e90ca55af3082791a5531
pranjalcr7007 <73832786+pranjalcr7007@users.noreply.github.com>
2020-12-13 15:17:38 +0530

    1.0.1

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,8 @@ class Post(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	private = models.BooleanField()
+
 
 	def __str__(self):
 		return self.title
@@ -21,19 +23,3 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
 
-class Postu(models.Model):
-	title = models.CharField(max_length=100)
-	file = models.FileField(null=True,blank=True,upload_to='Files')
-	content = models.TextField()
-	date_posted = models.DateTimeField(default=timezone.now)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.title
-
-	def extension(self):
-		name, extension = os.path.splitext(self.file.name)
-		return extension
-
-	def get_absolute_url(self):
-		return reverse('post-detailu', kwargs={'pk': self.pk})        
--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from .models import Post, Postu
-import operator
+from .models import Post
 from django.urls import reverse_lazy
 from django.contrib.staticfiles.views import serve
 
@@ -31,7 +30,8 @@ def getfile(request):
 
 
 class PostListView(ListView):
-    model = Postu
+ 
+    model = Post
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] 
@@ -53,27 +53,17 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-class PostDetailViewu(DetailView):
-    model = Postu
-    template_name = 'blog/post_detailu.html'
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content', 'file']
+    fields = ['title', 'content', 'file', 'private']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostCreateViewu(LoginRequiredMixin, CreateView):
-    model = Postu
-    template_name = 'blog/post_formu.html'
-    fields = ['title', 'content', 'file']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -91,20 +81,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
-class PostUpdateViewu(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Postu
-    template_name = 'blog/post_formu.html'
-    fields = ['title', 'content', 'file']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
@@ -117,16 +93,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
-class PostDeleteViewu(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Postu
-    success_url = '/'
-    template_name = 'blog/post_confirm_deleteu.html'
 
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
 
 
 
